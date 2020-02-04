cbf593a4416573c510eaf7fcc24af58ff8c6b5da
NUTTAPON BUABAN <61010345@kmitl.ac.th>
2020-02-04 07:33:09 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:momandkid/detailsPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -41,14 +42,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF21BFBD),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top:10.0,left:20.0) ,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
+      appBar: AppBar(
+        backgroundColor: Color(0xFF21BFBD),
+        elevation: 0.0,
+        actions: <Widget>[
                 IconButton(
+                  alignment: Alignment(-40.0,0.0),
                   icon: Icon(Icons.arrow_back_ios),
                   iconSize: 22.0,
                   color: Colors.white, 
@@ -73,8 +72,15 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                     ],
                   ),
-                ),  
-              ],
+                ),   
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top:10.0,left:20.0) ,
+            child: Row(
+              
             ),
           ),
           SizedBox(height: 25.0,),
@@ -102,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
-        SizedBox(height: 40.0,),
+        SizedBox(height: 100.0,),
         Container(
           height: MediaQuery.of(context).size.height - 185.0,
           decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0))),
@@ -115,17 +121,69 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Container(
                   height: MediaQuery.of(context).size.height - 300.0,
                   child: ListView(children:[
-                    
+                    _buildFoodItem('assets/plate1.png', 'Salmon bowl', '\$24.00'),
+                    _buildFoodItem('assets/plate2.png', 'Spring bowl', '\$22.00'),
+                    _buildFoodItem('assets/plate3.png', 'Avocado bowl', '\$26.00'),
+                    _buildFoodItem('assets/plate5.png', 'Berry bowl', '\$24.00'),
+                    _buildFoodItem('assets/plate4.png', 'Avocado bowl', '\$26.00'),
+                    _buildFoodItem('assets/plate6.png', 'Avocado bowl', '\$26.00'),
                     ],
                   ),
                 ),
-                )
+              ),
+              Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  height: 65.0,
+                  width: 60.0,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.grey,
+                        style: BorderStyle.solid,
+                        width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Center(
+                    child: Icon(Icons.search, color: Colors.black),
+                  ),
+                ),
+                Container(
+                  height: 65.0,
+                  width: 60.0,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.grey,
+                        style: BorderStyle.solid,
+                        width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Center(
+                    child: Icon(Icons.shopping_basket, color: Colors.black),
+                  ),
+                ),
+                Container(
+                  height: 65.0,
+                  width: 120.0,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.grey,
+                          style: BorderStyle.solid,
+                          width: 1.0),
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Color(0xFF1C1428)),
+                  child: Center(
+                      child: Text('Checkout',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              color: Colors.white,
+                              fontSize: 15.0))),
+                  )
+                ],
+              )
             ],
-
           ),
         )
-
-
         ],
       ),
     );
@@ -136,11 +194,52 @@ class _MyHomePageState extends State<MyHomePage> {
       child: InkWell(
         onTap: (){
           Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => DetailPage(heroTag : imgPath, foodName : foodName, foodPrice : price)
+            builder: (context) => DetailsPage(heroTag : imgPath, foodName : foodName, foodPrice : price)
           ));
         },
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: [
+                  Hero(
+                    tag: imgPath,
+                    child: Image(
+                      image: AssetImage(imgPath),
+                      fit: BoxFit.cover,
+                      height: 75.0,
+                      width: 75.0,
+                      )
+                  ),
+                  SizedBox(width: 10.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        foodName,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                        price,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 15.0,
+                          color: Colors.grey
+                        )
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            IconButton(icon: Icon(Icons.add), onPressed: () {},color: Colors.black
+            )
+          ],
         ),
       ),
     );
