0b5ce83876b6ff89f79286bde5133f8748f511e3
Nuttapon Buaban <50200295+9ine2543@users.noreply.github.com>
2020-04-29 17:10:19 +0700

    Add create post

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:momandkid/root_page.dart';
 import 'package:momandkid/services/auth.dart';
-import 'package:momandkid/Profile/profilePage.dart';
-import 'Profile/settingPage.dart';
+import 'post/createPost.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,8 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: RootPage(auth: Auth()),
-      home: mainProfile(),
+      home: RootPage(auth: Auth()),
+      // home: createPost(),
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
         fontFamily: 'Prompt',
