816062bae5554b258a6bc5655ee196d9ba68f656
Nuttapon Buaban <50200295+9ine2543@users.noreply.github.com>
2020-05-04 18:14:38 +0700

    เอากลับไปทำต่อซะนะบอย

--- lib/kids/healthMain.dart
+++ lib/kids/healthMain.dart
@@ -219,6 +219,8 @@ class _healthMainState extends State<healthMain> with TickerProviderStateMixin {
             Container(
               height: 100,
               child: appBar(
+                data: _kids.of(context).data,
+                userId: widget.userId,
                 kid: widget.data.getSelectedKid(),
                 open: _kids.of(context).open,
                 onTap: () {
@@ -785,31 +787,7 @@ class _childrenListState extends State<childrenList> {
                   ],
                 )),
           ),
-          GestureDetector(
-            onTap: () async {
-              var remove = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => editKidData(
-                            index: widget.index,
-                            data: widget.data.getKids()[widget.index],
-                            userId: widget.userId,
-                            dataList: widget.data,
-                          )));
-              if (remove == null) {
-                null;
-              } else {
-                setState(() {
-                  widget.data.getKids().removeAt(widget.index);
-
-                  widget.data.setSelectedKidAny();
-                  // widget.data.getSelectedKid());
-                });
-                // widget.data.getKids().removeAt(widget.index);
-              }
-            },
-            child: Icon(Icons.edit),
-          )
+          
         ],
       ),
     );
